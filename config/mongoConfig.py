50cc33e896170a8c564f311afb8b26a169fb7f71
Sayak Ghosh <gsayak22@gmail.com>
2024-09-03 14:31:58 +0530

    mongo

--- config/mongoConfig.py
+++ config/mongoConfig.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from config.config import MONGO_DB_URI
+from config.config import MONGO_DB_URI, MONGO_DB_NAME
 
 client_db = MongoClient(MONGO_DB_URI)
 db = client_db.get_database(MONGO_DB_NAME)
